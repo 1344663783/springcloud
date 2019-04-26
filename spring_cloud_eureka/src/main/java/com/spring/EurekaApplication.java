7acb9fbea789b61611314ab3c016e4928faa2414
zhangxiaoxiong <1344663783@qq.com>
2019-04-26 17:35:39 +0800

    服务注册

---
+++ spring_cloud_eureka/src/main/java/com/spring/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaApplication
+ * @Description TODO
+ * @Author zhangxiaoxiong
+ * @Date 2019/4/26 16:39
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
